819847278fab9fa36d5742e0fc5565c9204929e2
Cutie Deng <CutieDeng@iCloud.com>
2025-12-11 21:48:17 +0800

    (FEAT "server impl"
    )

---
+++ server.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require racket/date racket/tcp)
+
+(define LOG_PATH (build-path "server.debug"))
+(define LOG_PORT (open-output-file LOG_PATH #:exists 'append))
+
+(define (time/string) (date->string (current-date) #t))
+
+(define (handle/cmd-data data src-ip)
+  (fprintf LOG_PORT "((type command) (time ~s) (src-ip ~s) (cmd ~s))~n" (time/string) src-ip data) (flush-output LOG_PORT)
+  
+)
+
+(define (handle/unknown u src-ip)
+  (fprintf LOG_PORT "((type unknown) (time ~s) (sub-type ~s) (version ~s))~n" (time/string) (dict-ref u 'type) (dict-ref u 'version))
+)
+
+(define (handle/init output src-ip)
+  (fprintf LOG_PORT "((type init) (time ~s) (src-ip ~s))~n" (time/string) src-ip)
+  (call-with-input-file "welcome.sh" (lambda (input)
+    (copy-port (input output))))
+)
+
+(define (handle/tcp-connect input output)
+  (match-define-values (src-ip _) (tcp-addresses input))
+  (fprintf LOG_PORT "((type meta-connect) (time ~s) (src-ip ~s))~n" (time/string) src-ip) (flush-output LOG_PORT)
+  (define r (read input))
+  (define r^ (for/hash ([(k v) (in-dict r)]) (values k v)))
+  (match r^
+    [(hash 'version '(1 0 0) 'type 'command 'data data) (handle/cmd-data data src-ip)]
+    [(hash 'version '(1 0 0) 'type 'command 'data data 'duration duration) (handle/cmd-data data src-ip)]
+    [(hash 'version '(1 0 0) 'type 'init) (handle/init output src-ip)]
+    [(hash 'version _ 'type _ #:open) (handle/unknown r^ src-ip)]
+  )
+)
+
+(define tcp-server-thread (make-parameter #f))
+(define tcp-custodian (make-parameter #f))
+
+(define MAX_ALLOW_WAIT 128)
+
+(define (server-listen port-no)
+  (define cus (make-custodian))
+  (define t (thread (thunk (parameterize ([current-custodian cus]) (define listener (tcp-listen port-no MAX_ALLOW_WAIT)) (let loop ()
+    (define cus2 (make-custodian))
+    (parameterize ([current-custodian cus2])
+      (match-define-values (input output) (tcp-listen listener))
+      (thread (thunk (with-handlers ([exn:fail? (lambda (e) (custodian-shutdown-all cus2) (raise e))])
+        (handle/tcp-connect input output)
+        )
+        (custodian-shutdown-all cus2)))
+    )
+  )))))
+  (tcp-server-thread t)
+  (tcp-custodian cus)
+  t 
+)
+
+(define (server-close)
+  (kill-thread (tcp-server-thread))
+  (custodian-shutdown-all (tcp-custodian))
+)
